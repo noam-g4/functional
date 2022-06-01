5fd6cd352971f6306a1a47918d70a6ef7ef13940
noam-g4 <noam.g4@gmail.com>
2022-06-01 19:23:51 +0300

    move utils to slices.go and use them as exported funcs

--- slices.go
+++ slices.go
@@ -6,25 +6,39 @@ func EmptySet[T any]() []T {
 	return set
 }
 
+// checks if a slice is empty
+func IsEmptySlice[T any](s []T) bool {
+	if len(s) == 0 {
+		return true
+	}
+	return false
+}
+
+// takes a slice and an element and returns a copy
+// of the slice with the element appended to it
+func AppendToSlice[T any](y []T, x T) []T {
+	return append(y, x)
+}
+
 // takes a slice, function and empty EmptySet
 // and returns a new slice of which each element
 // has being passed through the function parameter
 func Map[T any](xs []T, f func(T) T, y []T) []T {
-	if isEmptySlice(xs) {
+	if IsEmptySlice(xs) {
 		return y
 	}
-	return Map(xs[1:], f, push(y, f(xs[0])))
+	return Map(xs[1:], f, AppendToSlice(y, f(xs[0])))
 }
 
 // takes a slice, predicate function and empty EmptySet
 // and returns a new slice of only the elements that have
 // resulted true when passed to the predicate function
 func Filter[T any](xs []T, p func(T) bool, y []T) []T {
-	if isEmptySlice(xs) {
+	if IsEmptySlice(xs) {
 		return y
 	}
 	if p(xs[0]) {
-		return Filter(xs[1:], p, push(y, xs[0]))
+		return Filter(xs[1:], p, AppendToSlice(y, xs[0]))
 	}
 	return Filter(xs[1:], p, y)
 }
@@ -34,7 +48,7 @@ func Filter[T any](xs []T, p func(T) bool, y []T) []T {
 // and returns the accumalated result,
 // govern by the parameter function
 func Reduce[T any](xs []T, f func(T, T) T, y T) T {
-	if isEmptySlice(xs) {
+	if IsEmptySlice(xs) {
 		return y
 	}
 	return Reduce(xs[1:], f, f(y, xs[0]))
@@ -43,7 +57,7 @@ func Reduce[T any](xs []T, f func(T, T) T, y T) T {
 // takes a single element and a slice
 // and checks if this element is in the slice
 func IsIn[T comparable](x T, xs []T) bool {
-	if isEmptySlice(xs) {
+	if IsEmptySlice(xs) {
 		return false
 	}
 	if x == xs[0] {
