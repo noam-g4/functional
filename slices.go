04624390a97a404d9bf097846efb843c3e4b611c
noam-g4 <noam.g4@gmail.com>
2022-09-07 13:17:56 +0300

    remove the need for passing empty slices as params

--- slices.go
+++ slices.go
@@ -1,11 +1,5 @@
 package functional
 
-// returns an empty slice of any type
-func EmptySet[T any]() []T {
-	set := []T{}
-	return set
-}
-
 // checks if a slice is empty
 func IsEmptySlice[T any](s []T) bool {
 	if len(s) == 0 {
@@ -23,24 +17,32 @@ func AppendToSlice[T any](y []T, x T) []T {
 // takes a slice, function and empty EmptySet
 // and returns a new slice of which each element
 // has being passed through the function parameter
-func Map[A, B any](xs []A, f func(A) B, y []B) []B {
+func Map[A, B any](xs []A, f func(A) B) []B {
+	return mapPrime(xs, f, make([]B, 0))
+}
+
+func mapPrime[A, B any](xs []A, f func(A) B, y []B) []B {
 	if IsEmptySlice(xs) {
 		return y
 	}
-	return Map(xs[1:], f, AppendToSlice(y, f(xs[0])))
+	return mapPrime(xs[1:], f, AppendToSlice(y, f(xs[0])))
 }
 
 // takes a slice, predicate function and empty EmptySet
 // and returns a new slice of only the elements that have
 // resulted true when passed to the predicate function
-func Filter[T any](xs []T, p func(T) bool, y []T) []T {
+func Filter[T any](xs []T, p func(T) bool) []T {
+	return filterPrime(xs, p, make([]T, 0))
+}
+
+func filterPrime[T any](xs []T, p func(T) bool, y []T) []T {
 	if IsEmptySlice(xs) {
 		return y
 	}
 	if p(xs[0]) {
-		return Filter(xs[1:], p, AppendToSlice(y, xs[0]))
+		return filterPrime(xs[1:], p, AppendToSlice(y, xs[0]))
 	}
-	return Filter(xs[1:], p, y)
+	return filterPrime(xs[1:], p, y)
 }
 
 // takes a slice, a function that takes an accumalator and
@@ -69,7 +71,5 @@ func IsIn[T comparable](x T, xs []T) bool {
 // takes two slices of the same type
 // and returns a concatanated slice [...s1, ...s2]
 func ConcatSlices[T any](s1, s2 []T) []T {
-	return Map(s2, func(x T) T {
-		return x
-	}, s1)
+	return append(s1, s2...)
 }
--- test/slices_test.go
+++ test/slices_test.go
@@ -1,35 +1,10 @@
 package functional
 
 import (
-	"reflect"
 	"testing"
 
 	f "github.com/noam-g4/functional"
 )
-
-func TestEmptySet(t *testing.T) {
-	intSet := f.EmptySet[int]()
-	floatSet := f.EmptySet[float32]()
-	boolSet := f.EmptySet[bool]()
-	stringSet := f.EmptySet[string]()
-	mapSet := f.EmptySet[map[string]interface{}]()
-
-	if reflect.TypeOf(intSet) != reflect.TypeOf([]int{}) {
-		t.Fail()
-	}
-	if reflect.TypeOf(floatSet) != reflect.TypeOf([]float32{}) {
-		t.Fail()
-	}
-	if reflect.TypeOf(boolSet) != reflect.TypeOf([]bool{}) {
-		t.Fail()
-	}
-	if reflect.TypeOf(stringSet) != reflect.TypeOf([]string{}) {
-		t.Fail()
-	}
-	if reflect.TypeOf(mapSet) != reflect.TypeOf([]map[string]interface{}{}) {
-		t.Fail()
-	}
-}
 
 func TestMap(t *testing.T) {
 	intSet := []int{1, 2, 3}
@@ -37,11 +12,11 @@ func TestMap(t *testing.T) {
 
 	intRes := f.Map(intSet, func(x int) int {
 		return x + 1
-	}, f.EmptySet[int]())
+	})
 
 	stringRes := f.Map(stringSet, func(s string) string {
 		return s + "!"
-	}, f.EmptySet[string]())
+	})
 
 	for n, x := range intSet {
 		if (x + 1) != intRes[n] {
@@ -74,7 +49,7 @@ func TestFilter(t *testing.T) {
 	stringSet := []string{"one", "two", "three", "four"}
 	filtered := f.Filter(stringSet, func(s string) bool {
 		return len(s) != 3
-	}, f.EmptySet[string]())
+	})
 
 	if f.IsIn("one", filtered) || f.IsIn("two", filtered) || len(filtered) != 2 {
 		t.Fail()
