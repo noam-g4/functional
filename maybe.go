8a3aa081fa6a175f479b9480af9db338aec70125
noam-g4 <noam.g4@gmail.com>
2022-09-07 11:26:37 +0300

    rm typo in comment

--- maybe.go
+++ maybe.go
@@ -36,7 +36,7 @@ and based on the result of the inner function, it returns an Either type
 with either an error or the value. the combinations of the Maybe and Either
 lets you handle error as in the following example:
 
-y := Maybe(Divide(5, 0)).HandleErr(log.Println).Value()
+y := Maybe(Divide(5, 0)).HandleErr(log.Println).Value
 */
 func Maybe[a any](err error, val a) Either {
 	if err != nil {
